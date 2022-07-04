d72818ae868b3fd671131a7bc520648639d98f11
AnhMNg <aminhng3012@gmail.com>
2022-07-04 16:11:28 +0700

    create config

---
+++ StockP/src/main/java/config/Config.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+/**
+ *
+ * @author Admin
+ */
+public class Config {
+    public static final String LAYOUT = "WEB-INF/layouts/main.jsp";
+}

7130dae53d46e36a05259e779a3452dab0e4d0c6
Phu <phieulinh341@gmail.com>
2022-07-18 16:52:19 +0700

    update cart

--- StockP/src/main/java/manager/ShoesManager.java
+++ StockP/src/main/java/manager/ShoesManager.java
@@ -41,7 +41,7 @@ public class ShoesManager {
                 shoes.setName(rs.getString("name"));
                 shoes.setImg(rs.getString("img"));
                 shoes.setPrice(rs.getFloat("price"));
-                shoes.setCategoryId(rs.getString("category_id"));               
+                shoes.setCategoryId(rs.getString("category_id"));
                 shoes.setSize(rs.getInt("size"));
                 shoes.setAmount(rs.getInt("amount"));
                 shoes.setCategoryName(rs.getString("categoryName"));
@@ -76,7 +76,7 @@ public class ShoesManager {
                 shoes.setName(rs.getString("name"));
                 shoes.setImg(rs.getString("img"));
                 shoes.setPrice(rs.getFloat("price"));
-                shoes.setCategoryId(rs.getString("category_id"));               
+                shoes.setCategoryId(rs.getString("category_id"));
                 shoes.setSize(rs.getInt("size"));
                 shoes.setAmount(rs.getInt("amount"));
                 shoes.setCategoryName(rs.getString("categoryName"));
@@ -89,7 +89,7 @@ public class ShoesManager {
         }
         return list;
     }
-    
+
     //get detail of shoes which are found
     public Shoes find(String id) {
         Shoes shoes = null;
@@ -105,7 +105,7 @@ public class ShoesManager {
                 shoes.setName(rs.getString("name"));
                 shoes.setImg(rs.getString("img"));
                 shoes.setPrice(rs.getFloat("price"));
-                shoes.setCategoryId(rs.getString("category_id"));               
+                shoes.setCategoryId(rs.getString("category_id"));
                 shoes.setSize(rs.getInt("size"));
                 shoes.setAmount(rs.getInt("amount"));
                 shoes.setCategoryName(rs.getString("categoryName"));
@@ -116,7 +116,7 @@ public class ShoesManager {
         }
         return shoes;
     }
-    
+
     public boolean checkShoes(String shoeId) throws SQLException {
         Connection con = DBUtil.getConnection();
         String sql = "select * from shoes where shoes_id = ?";
@@ -129,5 +129,34 @@ public class ShoesManager {
             }
         }
         return false;
+    }
+
+    public void updateShoesAmount(String shoeId, String amount) throws SQLException {
+        Connection con = DBUtil.getConnection();
+        try {
+            //creating and executing sql statements
+            String sql = " Select "
+                    + " "
+                    + " where s.id like ? ";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, shoeId);
+            ResultSet rs = stm.executeQuery();
+            //Loading data into the list
+            while (rs.next()) {
+                Shoes shoes = new Shoes();
+                shoes.setShoeId(rs.getString("shoes_id"));
+                shoes.setName(rs.getString("name"));
+                shoes.setImg(rs.getString("img"));
+                shoes.setPrice(rs.getFloat("price"));
+                shoes.setCategoryId(rs.getString("category_id"));
+                shoes.setSize(rs.getInt("size"));
+                shoes.setAmount(rs.getInt("amount"));
+                shoes.setCategoryName(rs.getString("categoryName"));
+            }
+            //closing the connection 
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ShoesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- StockP/src/main/webapp/WEB-INF/views/cart/index.jsp
+++ StockP/src/main/webapp/WEB-INF/views/cart/index.jsp
@@ -22,6 +22,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <<link rel="stylesheet" href="/css/button.cssf"/>
         <title>Cart Page</title>
     </head>
     <body>
